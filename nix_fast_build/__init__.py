87241a458ddf7e6ef50b01331df22112b51efeee
Michal Sojka <michal.sojka@cvut.cz>
2025-03-24 17:25:32 +0100

    Don't report negative durations

--- nix_fast_build/__init__.py
+++ nix_fast_build/__init__.py
@@ -816,7 +816,7 @@ async def run_builds(
                     result_type=ResultType.BUILD,
                     attr=job.attr,
                     success=rc == 0,
-                    duration=start_time - timeit.default_timer(),
+                    duration=timeit.default_timer() - start_time,
                     # TODO: add log output here
                     error=f"build exited with {rc}" if rc != 0 else None,
                 )
@@ -847,7 +847,7 @@ async def run_uploads(
                     result_type=ResultType.UPLOAD,
                     attr=build.attr,
                     success=rc == 0,
-                    duration=start_time - timeit.default_timer(),
+                    duration=timeit.default_timer() - start_time,
                     # TODO: add log output here
                     error=f"upload exited with {rc}" if rc != 0 else None,
                 )
@@ -872,7 +872,7 @@ async def run_cachix_upload(
                     result_type=ResultType.CACHIX,
                     attr=build.attr,
                     success=rc == 0,
-                    duration=start_time - timeit.default_timer(),
+                    duration=timeit.default_timer() - start_time,
                     error=f"cachix upload exited with {rc}" if rc != 0 else None,
                 )
             )
@@ -895,7 +895,7 @@ async def run_attic_upload(
                     result_type=ResultType.ATTIC,
                     attr=build.attr,
                     success=rc == 0,
-                    duration=start_time - timeit.default_timer(),
+                    duration=timeit.default_timer() - start_time,
                     error=f"attic upload exited with {rc}" if rc != 0 else None,
                 )
             )
@@ -919,7 +919,7 @@ async def run_downloads(
                     result_type=ResultType.DOWNLOAD,
                     attr=build.attr,
                     success=rc == 0,
-                    duration=start_time - timeit.default_timer(),
+                    duration=timeit.default_timer() - start_time,
                     error=f"download exited with {rc}" if rc != 0 else None,
                 )
             )
