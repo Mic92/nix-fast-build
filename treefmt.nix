adece9af60c76eaab265cfb32b39b707fc39562e
Jörg Thalheim <joerg@thalheim.io>
2025-05-04 06:14:06 +0200

    treefmt: disable riscv64

--- treefmt.nix
+++ treefmt.nix
@@ -9,11 +9,13 @@
         # Used to find the project root
         projectRootFile = ".git/config";
 
+        flakeCheck = pkgs.hostPlatform.system != "riscv64-linux";
+
         programs.deno.enable = pkgs.lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.deno;
         programs.mypy.enable = true;
         programs.mypy.directories."root".directory = ".";
         programs.deadnix.enable = true;
-        programs.nixfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
+        programs.nixfmt.enable = true;
         programs.ruff.format = true;
         programs.ruff.check = true;
       };
