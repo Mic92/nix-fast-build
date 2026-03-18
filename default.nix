db99344075840a592d461c87e0aa83f94235d097
Jörg Thalheim <joerg@thalheim.io>
2026-03-18 12:19:45 +0000

    bump version 1.4.0

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ let
 in
 python3Packages.buildPythonApplication {
   pname = "nix-fast-build";
-  version = "1.3.0";
+  version = "1.4.0";
   format = "pyproject";
   src = ./.;
   buildInputs = with python3Packages; [
